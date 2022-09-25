f974139f79d1a1fb2e96282d7a0579e13f25e331
guyaviv180 <guyaviv180@gmail.com>
2022-09-25 16:45:32 +0300

    first commit

---
+++ graphics.js
@@ -0,0 +1,61 @@
+function drawRectangle(context, x, y, width, height, color, borderWidth, borderColor) {
+    context.beginPath();
+    context.rect(x, y, width, height);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+    context.strokeStyle = "black"
+    context.lineWidth = 1;
+}
+
+function drawLine(context, x1, y1, x2, y2, width, color) {
+    context.beginPath();
+    context.lineWidth = width;
+    context.strokeStyle = color;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+}
+
+function drawText(context, x, y, sizeAndFont, color, text) {
+    context.beginPath();
+    context.font = sizeAndFont;
+    context.fillStyle = color;
+    context.fillText(text, x, y);
+    context.stroke;
+    context.fillStyle = "black";
+}
+
+function drawArc(x, y, r, color, borderWidth, borderColor) {
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+    context.strokeStyle = color
+    context.lineWidth = 1;
+
+}
+
+function drawTriangle() {
+    context.beginPath();
+    context.moveTo(xOfFirstPoint, yOfFirstPoint);
+    context.lineTo(xOfSecondPoint, yOfSecondPoint);
+    context.lineTo(xOFThirdPoint, yOFThirdPoint);
+    context.lineTo(xOfFirstPoint, yOfFirstPoint);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
